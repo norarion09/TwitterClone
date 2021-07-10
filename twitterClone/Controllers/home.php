bf9b534230a26d87198048718e49c93fc0155747
norarion09 <nukomaru9@gmail.com>
2021-07-10 11:02:21 +0900

    ログイン機能途中

---
+++ twitterClone/Controllers/home.php
@@ -0,0 +1,14 @@
+<?php 
+///////////////////////
+//ホームコントローラー
+///////////
+
+//設定を読み込み
+include_once '../config.php';
+//便利な関数を読み込み
+include_once '../util.php';
+
+//TODO:ログインしているか
+
+//画面表示
+include_once '../views/home.php';
